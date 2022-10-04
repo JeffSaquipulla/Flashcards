ab88fb3b10fa126ea7b8b9a607c74431ac12aa2a
Jeff Saquipulla <jeff.saquipulla12@gmail.com>
2022-10-04 02:39:37 -0400

    i was able to completed 85% of lab2. Completed up to STEP 3 part 4. When I ran my code, the ‘+’ button on the view controller doesn’t allowed me to go to the creation view controller anymore

---
+++ Flashcards/CreationViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Jeff Saquipulla on 10/2/22.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+    
+    var flashcardsController: ViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss (animated: true)
+        //Void dismiss(animated: Bool, completion: (()->Void)?)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        
+        let questionText = questionTextField.text
+        
+        let answerText = answerTextField.text
+        
+        flashcardsController.updateFlashcard(question: questionText!, answer: answerText!)
+        
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,16 +12,20 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
-    
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         frontLabel.isHidden = true
     }
+    
+    func updateFlashcard(question: String, answer: String) {
+       //updateFlashcard(question: "Is the moon considered a star ", answer: "yes it is")
+    }
+    
     
     
 }
